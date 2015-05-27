1288787494e45a3ba24474d2bd74378adfdec3f3
AKIRA-MIAYEK <akira.aktk.miyake@gmail.com>
2015-05-27 22:42:03 +0900

    Initial Commit

---
+++ OpenWeatherMapper/OpenWeatherMapper.h
@@ -0,0 +1,19 @@
+//
+//  OpenWeatherMapper.h
+//  OpenWeatherMapper
+//
+//  Created by MiyakeAkira on 2015/05/27.
+//  Copyright (c) 2015年 Miyake Akira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenWeatherMapper.
+FOUNDATION_EXPORT double OpenWeatherMapperVersionNumber;
+
+//! Project version string for OpenWeatherMapper.
+FOUNDATION_EXPORT const unsigned char OpenWeatherMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenWeatherMapper/PublicHeader.h>
+
+
