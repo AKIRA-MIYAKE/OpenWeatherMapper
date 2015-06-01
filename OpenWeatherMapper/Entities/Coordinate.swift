e4903e2899e9cd8605cd5f2a61780a3575f21dd2
AKIRA-MIAYEK <akira.aktk.miyake@gmail.com>
2015-06-01 23:57:56 +0900

    CoordinateをHashableに適用

--- OpenWeatherMapper/Entities/Coordinate.swift
+++ OpenWeatherMapper/Entities/Coordinate.swift
@@ -8,14 +8,22 @@
 
 import Foundation
 
-public struct Coordinate {
+public struct Coordinate: Hashable {
     
     public let latitude: Double
     public let longitude: Double
+    
+    public var hashValue: Int {
+        return Int(latitude * longitude)
+    }
     
     public init(latitude: Double, longitude: Double) {
         self.latitude = latitude
         self.longitude = longitude
     }
     
+}
+
+public func ==(lhs: Coordinate, rhs: Coordinate) -> Bool {
+    return lhs.hashValue == rhs.hashValue
 }
